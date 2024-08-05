c5931f853295ba97a0d61d7a39b3694d4dd1aca1
Yaroslav Melnychenko <ym@empat.tech>
2024-08-05 22:53:11 +0300

    Added health api endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
