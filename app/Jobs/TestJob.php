d257973967c248f32042b1fc2c0b36032857b4c8
Yaroslav Melnychenko <ym@empat.tech>
2025-05-27 12:17:58 +0300

    Add TestJob for queue processing and new job dispatch route

---
+++ app/Jobs/TestJob.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
+
+class TestJob implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::alert('Hello! I`m a job from queue');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\TestJob;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/health', function () {
@@ -10,4 +11,10 @@ Route::get('/error', function () {
     throw new \Exception('My exception', rand(1000, 1999));
 
     return response()->json(['status' => 'ok']);
+});
+
+Route::get('/job', function () {
+    TestJob::dispatch();
+
+    return response()->json(['status' => 'dispatched']);
 });
